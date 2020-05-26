48db98bdb15db976ffe37c66dfce1faaebed61ff
Amin Rabhi <aminrhrh@gmail.com>
2020-05-26 20:09:28 +0100

    commit mybatis

--- tom-mobile-service/src/main/java/com/tn/itserv/tom/services/UtilisateurService.java
+++
@@ -1,8 +0,0 @@
-package com.tn.itserv.tom.services;
-
-import com.tn.itserv.tom.entities.Utilisateur;
-
-public interface UtilisateurService {
-	
-	public Utilisateur getUserByLogin(String login) ;
-}
